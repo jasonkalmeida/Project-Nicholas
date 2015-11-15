cb764d48e8cb981ef5fbf540a78dc8c8e6986066
mkoch17 <koch.382@osu.edu>
2015-11-14 22:57:16 -0500

    Chrome Extension
    Chrome extension that takes in the URL when the button is pressed and
    displays a message to the user

---
+++ Mason's version/background.js
@@ -0,0 +1,16 @@
+
+//User Clicks the action
+chrome.browserAction.onClicked.addListener(function(tab){
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		var activeTab = tabs[0];
+		var url = activeTab.url;
+		var opt = {
+			type: "basic",
+			title: "Saved!",
+			message: "We'll let you know if the price drops!",
+			iconUrl: "icon3.png"
+		}
+		alert(opt.iconUrl);
+		chrome.notifications.create(opt);
+	});
+});

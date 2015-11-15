0630622b0a5c18c1f6895ba6a08bc89f509ab3c0
daniellebeckley <beckley.52@osu.edu>
2015-11-15 09:12:01 -0500

    can be called once, will update all file
    can be called once, will update all file

--- Ruby/update.rb
+++ Ruby/update.rb
@@ -1,20 +1,24 @@
+#can only call this once, otherwise need to delete last line ]}
 require_relative 'Amazon'
 require_relative 'Forever21'
 require_relative 'Hm'
 require_relative 'Macy'
 require_relative 'Rue21'
 require_relative 'Tobi'
+require 'json'
+
+
+File.open("to.json","a") do |f|
+	f.write("]}")
+end
 
 file = File.read('to.json')
 data_hash = JSON.parse(file)
-
 y =  data_hash["employees"]
-
-File.open("deal.json","a") do |f|
+File.open("deal.json","w") do |f|
 	f.write('{"employees":[ ')
 	f.write("\n")
 end
-
 happened = nil;
 
 y.each do |entry| 
@@ -25,35 +29,37 @@ y.each do |entry|
 	case website
 
 	when "Amazon.com"
-		puts "Amazon.com"
+		#puts "Amazon.com"
 		x = Amazon_scraper.new(d)
 		check_price = x.price
 
 	when "Forever21"
-		puts "Forever21"
+		#puts "Forever21"
 		x = Forever21_scraper.new(d)
 		check_price = x.price
 	when "H&M"
-		puts "H&M"
+		#puts "H&M"
 		x = Hm_scraper.new(d)
 		check_price = x.price
 	when "Macy's"
-		puts "Macy's"
+		#puts "Macy's"
 		x = Macy.new(d)
 		check_price = x.price
 
 	when "rue21"
-		puts "rue21"
+		#puts "rue21"
 		x = Rue21_scraper.new(d)
 		check_price = x.price
+	
 	when "Tobi"
-		puts "Tobi"
+		#puts "Tobi"
 		x = Tobi_scraper.new(d)
 		check_price = x.price
 	else "Incorrect Something"
 
 	end
 	if(check_price < current_price)
+		#puts "DEAL!"
 		args = {
 			:site => website,
 			:product => entry["product"],
@@ -62,7 +68,7 @@ y.each do |entry|
 		}
 		
 		all_sites = args;
-		puts "DEAL!"
+		
 		#need to format file
 		File.open("deal.json","a") do |f|
 			if(happened)
@@ -74,10 +80,9 @@ y.each do |entry|
 		happened = true;
 		
 		else
-			puts "Same price or greater"
+		#	puts check_price
 		end
 end
 File.open("deal.json","a") do |f|
 	f.write("]}")
 end
-

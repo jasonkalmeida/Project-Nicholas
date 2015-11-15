658084a255ac3b7edbf528a6577097b9ef24aff3
daniellebeckley <beckley.52@osu.edu>
2015-11-15 10:32:45 -0500

    update
    update

---
+++ Ruby/test.rb
@@ -0,0 +1,38 @@
+require_relative 'add'
+require 'json'
+
+websites = [
+	
+	"http://www.forever21.com/Product/Product.aspx?BR=f21&Category=dress&ProductID=2000146561&VariantID=",
+	"http://www.hm.com/us/product/19317?article=19317-D",
+	"http://www1.macys.com/shop/product/ny-collection-b-slim-three-quarter-sleeve-dress?ID=579770&CategoryID=5449&LinkType=#fn=SPECIAL_OCCASIONS%3DWear%20to%20Work%26SPECIAL_OFFERS%3DSales%20&amp;%20Discounts%26sp%3D1%26spc%3D522%26ruleId%3D%26slotId%3D5",
+	"http://www.rue21.com/store/jump/product/Light-Blue-Static-Tee/0252-001110-0002121-0041",
+	"http://www.tobi.com/product/57830-tobi-love-phenom-dress?color_id=87256"
+]
+
+=begin
+websites.each do |x|
+	x = Addsite.new(x)
+	#puts x.all_sites
+end
+=end
+
+x = Addsite.new('http://www.forever21.com/Product/Product.aspx?BR=f21&Category=sweater&ProductID=2000163400&VariantID=')
+=begin File.open("to.json","a") do |f|
+			f.write("]}")
+			puts f.size();
+end
+
+=end
+
+#JASON NEED TO DELETE LAST LINE AND ADD A COMMA, ADD WHAT THE USER WANTS AND THEN DO "\}"
+=begin
+file = File.read('to.json')
+data_hash = JSON.parse(file)
+
+=begin 
+x =  data_hash.keys
+y =  data_hash["employees"]
+l = y[0]
+puts l["site"]
+=end
--- Ruby/update.rb
+++ Ruby/update.rb
@@ -19,6 +19,7 @@ File.open("deal.json","w") do |f|
 	f.write('{"employees":[ ')
 	f.write("\n")
 end
+
 happened = nil;
 
 y.each do |entry| 
@@ -29,60 +30,61 @@ y.each do |entry|
 	case website
 
 	when "Amazon.com"
-		#puts "Amazon.com"
-		x = Amazon_scraper.new(d)
-		check_price = x.price
+				#puts "Amazon.com"
+				x = Amazon_scraper.new(d)
+				check_price = x.price
 
-	when "Forever21"
-		#puts "Forever21"
-		x = Forever21_scraper.new(d)
-		check_price = x.price
-	when "H&M"
-		#puts "H&M"
-		x = Hm_scraper.new(d)
-		check_price = x.price
-	when "Macy's"
-		#puts "Macy's"
-		x = Macy.new(d)
-		check_price = x.price
+			when "Forever21"
+				#puts "Forever21"
+				x = Forever21_scraper.new(d)
+				check_price = x.price
+			when "H&M"
+				#puts "H&M"
+				x = Hm_scraper.new(d)
+				check_price = x.price
+			when "Macy's"
+				#puts "Macy's"
+				x = Macy.new(d)
+				check_price = x.price
 
-	when "rue21"
-		#puts "rue21"
-		x = Rue21_scraper.new(d)
-		check_price = x.price
-	
-	when "Tobi"
-		#puts "Tobi"
-		x = Tobi_scraper.new(d)
-		check_price = x.price
-	else "Incorrect Something"
+			when "rue21"
+				#puts "rue21"
+				x = Rue21_scraper.new(d)
+				check_price = x.price
 
-	end
-	if(check_price < current_price)
-		#puts "DEAL!"
-		args = {
-			:site => website,
-			:product => entry["product"],
-			:price => check_price,
-			:url => d
-		}
-		
-		all_sites = args;
-		
-		#need to format file
-		File.open("deal.json","a") do |f|
-			if(happened)
-				f.write(",")
+			when "Tobi"
+				#puts "Tobi"
+				x = Tobi_scraper.new(d)
+				check_price = x.price
+			else "Incorrect Something"
+
 			end
+			if(check_price < current_price)
+			#puts "DEAL!"
+			args = {
+				:site => website,
+				:product => entry["product"],
+				:price => check_price,
+				:url => d
+			}
+			
+			all_sites = args;
+			
+			#need to format file
+			File.open("deal.json","a") do |f|
+				if(happened)
+					f.write(",")
+				end
 				f.write(all_sites.to_json)
 				f.write("\n")
-		end
-		happened = true;
-		
+			end
+			happened = true;
+			
 		else
-		#	puts check_price
+			#	puts check_price
 		end
-end
-File.open("deal.json","a") do |f|
-	f.write("]}")
-end
+	end
+	File.open("deal.json","a") do |f|
+		f.write("]}")
+	end
+
